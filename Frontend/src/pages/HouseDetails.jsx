5ff5f425a739050f6ca5da62363d200109ffefaa
destuinystarboy33-pro <destinystarboy33@gmail.com>
2026-09-01 16:11:14 +0100

    changed some colors

--- Frontend/src/pages/HouseDetails.jsx
+++ Frontend/src/pages/HouseDetails.jsx
@@ -69,7 +69,7 @@ const [properties, setProperties] = useState([]);
           <p className="text-2xl">{House.distance} away from Bustop</p>
 
           <div className="flex gap-4 text-2xl mt-5">
-            <del className="text-red-300 font-extrabold">${House.oldPrice.toLocaleString()}</del>
+            <del className="text-red-600 font-extrabold">${House.oldPrice.toLocaleString()}</del>
             <p className="text-green-600 font-extrabold">${House.price.toLocaleString()}</p>
           </div>
           <p className="text-3xl font-medium mt-15">{House.desc}</p>
